b0803afc7a4d1478cc769ba8276dbba9bc74ed97
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-04 22:43:19 +0530

    Add files via upload

---
+++ Number_position.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int key = 55;
+    int x[5] = {34, 45, 65, 33, 55};
+    int i;
+    for (i = 0; i < 5; i++) {
+        if (x[i] == key) {
+            cout << "Element is found at position " << i << endl;
+            break;
+        }
+    }
+    if (i == 5) {
+        cout << "Element is not found in the array" << endl;
+    }
+}
