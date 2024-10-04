3c8361396282e994c5c6d30cb55f8bbb6c0fd41d
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-04 20:36:30 +0530

    Add files via upload

---
+++ String_Basics.cpp
@@ -0,0 +1,14 @@
+//Basics Of String
+#include <iostream>
+using namespace std;
+int main(){
+    char str1[4]="C++";
+    char str2[]={'C',' ','+','+','\0'};
+    char str3[4]={'C','+','+','\0'};
+    string str4 ="C++";
+    cout<<str1<<endl;
+    cout<<str2<<endl;
+    cout<<str3<<endl;
+    cout<<str4<<endl;
+}
+
