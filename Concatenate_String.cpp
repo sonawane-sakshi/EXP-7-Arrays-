263ff41024f08acae57bd7cc01324176149d6435
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-04 21:23:38 +0530

    Add files via upload

---
+++ Concatenate_String.cpp
@@ -0,0 +1,12 @@
+//Sakshi Sonawane
+//PRN:24070123505
+#include <iostream>
+using namespace std;
+int main(){
+    string a,b;
+    cout<<"Enter first sring: "<<endl;
+    cin>>a;
+    cout<<"Enter second sring: "<<endl;
+    cin>>b;
+    cout<<"Concatenated String is: "<<a+b<<endl;
+}
